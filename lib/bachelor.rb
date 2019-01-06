ab2aac59cdc5c4e7ce1bb8209b129c97d167fc1b
Joe Mullen <mullenjoe13@gmail.com>
2019-01-06 05:01:39 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -3,7 +3,7 @@ def get_first_name_of_season_winner(data, season)
     if contestant_hash["status"].downcase == "winner"
       return contestant_hash["name"].split(" ").first
     end
-  endend
+  end
 
 def get_contestant_name(data, occupation)
   # code here
